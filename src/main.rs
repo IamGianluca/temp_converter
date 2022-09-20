098d2b70c38f49e93b37a3a1a3b1a257ec32f3e1
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2022-09-19 21:36:45 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::io;
+use math::round;
+
+fn main() {
+    println!("Hello, world!");
+
+    let mut f_temp = String::new(); // reaD_line() read input as strings
+
+    println!("Please enter temperature in Fahrenheit");
+    io::stdin()
+        .read_line(&mut f_temp)
+        .expect("Error");
+
+    let f_temp: f64 = f_temp
+        .trim()
+        .parse()
+        .expect("Something went wrong");
+
+    let c_temp = round::half_away_from_zero((f_temp - 32.) / 1.8, 2);
+    println!("The temperature in Celsius is {c_temp}");
+}
